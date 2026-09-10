bdfa20e0084e343fece8ad346056bd1a700eac87
gaeksgroup-hash <gaeks.group@gmail.com>
2026-09-10 14:20:25 +0000

    chore: configure paths for manual PNG upload (favicon.png, gaek-symbol.png, gaek-full.png)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ export const Navbar: React.FC<NavbarProps> = ({
           aria-label="Gaek Freight Home"
         >
           <img 
-            src="/logos/gaek-symbol.svg" 
+            src="/logos/gaek-symbol.png" 
             onError={(e) => {
               (e.currentTarget as HTMLImageElement).src = '/logos/logo-gaek-symbol-only.jpg';
             }}
